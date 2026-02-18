5038218c33925d30dd360da8fa59cd6bd37c9a7f
Fares-H2811 <faresh2811@gmail.com>
2026-02-18 11:28:10 +0200

    portfolio V 2.0

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-const texts = [
-    "Electronics Engineer",
-    "Data Analyst",
-    "AI Enthusiast",
-    "Future Innovator"
-  ];
-  
-  let speed = 100;
-  let index = 0;
-  let charIndex = 0;
-  let isDeleting = false;
-  
-  const typingElement = document.getElementById("typing");
-  
-  function typeEffect() {
-  
-    let currentText = texts[index];
-  
-    if (isDeleting) {
-      typingElement.textContent = currentText.substring(0, charIndex--);
-    } else {
-      typingElement.textContent = currentText.substring(0, charIndex++);
-    }
-  
-    if (!isDeleting && charIndex === currentText.length) {
-      isDeleting = true;
-      setTimeout(typeEffect, 1200);
-      return;
-    }
-  
-    if (isDeleting && charIndex === 0) {
-      isDeleting = false;
-      index = (index + 1) % texts.length;
-    }
-  
-    setTimeout(typeEffect, isDeleting ? speed / 2 : speed);
-  }
-  
-  typeEffect();
-  
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+          behavior: "smooth"
+      });
+  });
+});
